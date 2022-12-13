b2f0e7051922e1e4aec0d6e214b8d6a7ce1a6555
FelipeL58 <felip_58@hotmail.com>
2022-12-12 21:40:09 -0500

    Version9

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -90,7 +90,8 @@ const routes = [
         image={IntPotencia}
         title="Interruptor de Potencia"
         label=""
-        description="Interruptor de potencia es un dispositivo capaz de interrupmpir el flujo de corriente, bien sea en operación normal o en corto circuito..."
+        description="Interruptor de potencia es un dispositivo capaz de interrupmpir el flujo de corriente, bien sea en operación normal o en corto circuito. Cuenta con un número límite de aperturas y cierres, 
+        este valor difiere si el equipo actúa bajo condiciones normales o de falla."
       />
     ),
   },
@@ -119,8 +120,9 @@ const routes = [
             ]}
             image={ExtAire}
             title="AIRE"
-            label="Aire"
-            description="El interruptor de aire usado anteriormente, a través del tiempo..."
+            label=""
+            description="Estos interruptores utilizan  el aire como medio de interrupción a presión atmosférica, logrando des-ionizar el arco eléctrico por la acción enfriadora que caracteriza este elemento. El arco producido en la cámara de interrupción puede estirarse y así aumentar su resistencia por longitud y confinamiento, logrando que la corriente de cortocircuito descienda y que la corriente y el voltaje entren en fase. Una vez se reduce la diferencia entre la tensión del sistema y la corriente de corto, se tendrá la seguridad que el voltaje de restablecimiento tendrá un valor bajo.
+            Debido a que la energía disipada es tan elevada, su aplicación se limita a mediana y baja potencia."
           />
         ),
       },
@@ -155,7 +157,8 @@ const routes = [
                 type="video"
                 title="Aceite"
                 label="Gran Volumen"
-                description="Interruptor de gran volumen, por su alta capacidad de contener aceite, lo hace un medio de interrupción peligroso en caso de una explosión..."
+                description="Este tipo de interruptores usan la reacción del aceite al momento de producirse un arco para su propia extinción. El arco eléctrico provoca que se rompan las moléculas del aceite generando hidrogeno y otros gases, además de, una especie de turbulencia en el aceite dentro del tanque o cámara de extinción, dichas reacciones químicas y físicas son las que enfrían y comprimen el plasma del arco hasta su des-ionisación.
+                Como su nombre lo indica este tipo de interruptores de potencia alberga en la cámara de extinción una cantidad importe de aceite, ejemplo, para interruptores de 145 kV la cantidad de aceite es en promedio de 12000 litros, para un interruptore de 230 kV se necesitarían aproximadamente 50000 litros."
                 action={{
                   type: "external",
                   route: "http://localhost:8080/Interruptor de aceite.html",
@@ -188,7 +191,7 @@ const routes = [
                 image={PeqVolAceite}
                 title="Aceite"
                 label="Pequeño Volumen"
-                description="Interruptor de pequeño volumen, en cuanto a espacio es mas sofisticado que el de gran volumen..."
+                description="Debido la cantidad de espacio que ocupa los interruptores de gran volumen de aceite se diseñó un modelo con menos cantidad de este mineral. Los interruptores  de pequeño volumen contienen aproximadamente el 2% de aceite que usa un interruptor de gran volumen. La forma de operación de ambos interruptores es similar, la diferencia radica en que los de pequeño volumen de aceite usan el aceite solo como medio de interrupción y adicional a este tienen materiales solidos usados como dieléctricos; el interruptor de gran volumen usa el aceite como dieléctrico y como medio interruptivo."
               />
             ),
           },
@@ -213,7 +216,8 @@ const routes = [
             image={VacioImg}
             title="Vacío"
             label="Vacío"
-            description="Interruptor de vacío, su uso tecnológico cada vez es mas usado hasta media tensión..."
+            description="El vacío es un ambiente con una alta capacidad dieléctrica contenida en una cámara sellada permanentemente, esto permite que la distancia de los contactos sean pequeñas, de acuerdo a la capacidad del interruptor la separación de los contactos puede estar entre los 6 mm hasta 25 mm.  Presenta una rápida interrupción del arco desde el momento en que se separan los contactos, con la interrupción del arco la fuerza dieléctrica del vacío aumenta mucho mas en comparación a otros medios dieléctricos.
+            Los interruptores de vacío son altamente eficiente, de pequeño tamaño y menor costo, el vacío no es un dieléctrico que requiera de mantenimientos periódicos, lo que lo representa como un tipo de interruptor con una vida útil alta además de ser compactos y confiables . Sin embargo, estos interruptores pueden ser propensaos a reencendidos e interrupciones prematuras de pequeñas corrientes inductivas."
           />
         ),
       },
@@ -237,7 +241,10 @@ const routes = [
             image={SF6}
             title="SF6"
             label="SF6"
-            description="Interruptor en SF6, junto al de vacío son los interruptores más utilizados, con la diferencia que este está destinado también para alta tensión..."
+            description="El uso del gas sf6 nace de la necesidad de encontrar mejores características disruptivas de un fluido extintor frente al aumento constante de la potencia de las centrales eléctricas.
+            El gas SF6 o hexafluoruro es una gas halógeno, su estructura molecular se compone de un átomo de azufre central unido a 6 átomos de flúor. Posee una gran capacidad dieléctrica como medio de extinción del arco eléctrico. Además es incoloro, inodoro, no inflamable ni toxico. A temperatura estándar de 20 °c y presión atmosférica, este gas posee una densidad cinco veces superior a la del aire, haciéndolo uno de los gases mas pesados, sin embargo, sus características hacen que sea un compuesto químico muy estables.
+            Frente a otros medios de extinción el gas tiene varias ventajas: Menor tamaño y mas eficiencia en los interruptores, no genera residuos de carbono como el uso del aceite que se desgasta en cada proceso extintor y, mayor densidad y rigidez que el aire.
+            "
           />
         ),
       },
@@ -281,7 +288,7 @@ const routes = [
                 type="video"
                 title="Tanque Vivo"
                 label="Monopolar"
-                description="Interruptor Tanque Vivo Monopolar, su nombre se debe a dos cosas..."
+                description="La cámara de extinción se encuentra dentro del tanque aislante o polos del interruptor.  Estos polos van conectados directamente al circuito de alta tensión."
                 action={{
                   type: "external",
                   route: "http://localhost:8080/Tanque vivo Monopolar.html",
@@ -358,7 +365,14 @@ const routes = [
                 type="video"
                 title="Tanque Muerto"
                 label="Monopolar"
-                description="Interruptor Tanque Muerto Monopolar, ..."
+                description="La principal característica de los interruptores de potencia de tanque muerto es que poseen una cámara de interrupción encapsulada la cual está puesta a tierra; de ahí su nombre. 
+                Principales características de este tipo de interruptores:
+                Poseen la capacidad de realizar corto de corriente de cortocircuito de alta intensidad con sólo una cámara de interrupción.
+                La cámara encapsulada representa un alto grado de disponibilidad del servicio.
+                Menor número de mantenimiento.
+                Diseño compacto debido a transformadores de corriente de núcleo toroidal en los aisladores pasantes.
+                Mayor resistencia sísmica, debido al centro de gravedad.
+                "
                 action={{
                   type: "external",
                   route: "http://localhost:8080/Tanque muerto Monopolar.html",
